1d03bbe88f1db32945dc47ed98149a6d6170ec50
haideralirajput <160247584+haideralirajput@users.noreply.github.com>
2024-10-03 20:36:30 +0500

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,8 +1,8 @@
 // pages/index.js
-import NavBar from '../components/NavBar';
-import HeroSection from '../components/HeroSection';
-import FeatureSection from '../components/FeatureSection';
-import FeaturedTemplates from '../components/FeaturedTemplates';
+import NavBar from './components/NavBar';
+import HeroSection from './components/HeroSection';
+import FeatureSection from './components/FeatureSection';
+import FeaturedTemplates from './components/FeaturedTemplates';
 
 export default function Home() {
   return (
